93925619adbafda41e3f63588136870cab57caad
mingyeongmo <mingyeongmp@naver.com>
2021-08-30 21:09:07 +0900

    typescript_implement

--- src/index3.js
+++ src/index3.js
@@ -1,2 +1,44 @@
-let age3 = 6;
-console.log(age3);
+const add3 = function (x, y) {
+    return x + y;
+};
+add3(10, 20);
+const age2 = (age) => {
+    return age > 19; // 크면 true 아니면 false
+};
+age2(33); // true
+console.log(age2);
+// implements
+// interface Car {
+//     color: string;
+//     wheels: number;
+//     start(): void;
+// }
+// interface Benz extends Car {
+//     door: number;
+//     stop(): void;
+// }
+// const benz : Benz = {
+//     door : 5,
+//     stop() {
+//         console.log('stop');
+//     },
+//     color: 'black',
+//     wheels : 4,
+//     start() {
+//         console.log('go..');
+//     }
+// }
+// class Bmw2 implements Car {
+//     color;
+//     wheels = 4;
+//     constructor(c:string){
+//         this.color = c;
+//     }
+//     start() {
+//         console.log('go..');
+//     }
+// }
+// const bb = new Bmw2('green');
+// console.log(bb);
+// bb.start();
+// extends
--- src/index3.ts
+++ src/index3.ts
@@ -1,2 +1,66 @@
-let age3 = 6;
-console.log(age3);
+interface Add {
+    (num1:number, num2: number): number;
+}
+
+const add3 : Add = function(x,y){
+    return x + y;
+}
+
+add3(10,20);
+
+
+
+interface IsAdult {
+    (age:number): boolean;
+}
+
+const age2:IsAdult = (age) => {
+    return age > 19; // 크면 true 아니면 false
+}
+
+age2(33) // true
+console.log(age2);
+
+// implements
+
+// interface Car {
+//     color: string;
+//     wheels: number;
+//     start(): void;
+// }
+
+// interface Benz extends Car {
+//     door: number;
+//     stop(): void;
+// }
+
+// const benz : Benz = {
+//     door : 5,
+//     stop() {
+//         console.log('stop');
+//     },
+//     color: 'black',
+//     wheels : 4,
+//     start() {
+//         console.log('go..');
+//     }
+
+// }
+
+// class Bmw2 implements Car {
+//     color;
+//     wheels = 4;
+
+//     constructor(c:string){
+//         this.color = c;
+//     }
+//     start() {
+//         console.log('go..');
+//     }
+// }
+
+// const bb = new Bmw2('green');
+// console.log(bb);
+// bb.start();
+
+// extends
